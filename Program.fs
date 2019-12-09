246ee7061c9cbd4ae9e1395bb4e85d683db08e20
Chris Hyndman <chrishyndman@outlook.com>
2019-12-09 00:18:38 -0800

    add license headers

--- Program.fs
+++ Program.fs
@@ -1,4 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿(* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. *)
 
 open System
 
