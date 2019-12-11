f3d47d69151fb043ef094d3897fa7e5d701eef81
Chris Hyndman <chrishyndman@outlook.com>
2019-12-11 00:47:11 -0800

    finish day 1

--- Day1.fs
+++ Day1.fs
@@ -4,17 +4,34 @@
 
 module AdventOfCode.Day1
 
+    let private solveSumOf f =
+        (Seq.fold (fun acc l -> acc + (f (int l))) 0) >> (fun i -> Some i)
+
     let private fuel mass = (mass / 3) - 2
 
-    let kat () =
+    let private katPart1 () =
         Kat.init()
         |> Kat.expect 654 (fuel 1969)
         |> Kat.expect 33583 (fuel 100756)
         |> Kat.finish
 
-    let solve =
-        (Seq.fold (fun acc l -> acc + (fuel (int l))) 0) >> (fun i -> Some i)
+    let private solvePart1 = solveSumOf fuel
+
+    let rec private fuelCompound mass =
+        let f = fuel mass
+        if f > 0 then f + (fuelCompound f) else 0
+
+    let private katPart2 () =
+        Kat.init()
+        |> Kat.expect 2 (fuelCompound 14)
+        |> Kat.expect 966 (fuelCompound 1969)
+        |> Kat.expect 50346 (fuelCompound 100756)
+        |> Kat.finish
+
+    let private solvePart2 = solveSumOf fuelCompound
+
     let puzzles =
         [
-            { Puzzle.Name = "day1"; KnownAnswerTest = kat; Solver = solve }
+            { Puzzle.Name = "day1p1"; KnownAnswerTest = katPart1; Solver = solvePart1 }
+            { Puzzle.Name = "day1p2"; KnownAnswerTest = katPart2; Solver = solvePart2 }
         ]
